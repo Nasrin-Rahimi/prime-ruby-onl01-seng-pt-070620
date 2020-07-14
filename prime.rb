41f9a9af2d2c1ef01787c6e51a1b78e145563ada
Nasrin Rahimi <77.nasrin@gmail.com>
2020-07-14 19:32:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,10 @@
 def prime?(number)
-  (2..(number - 1)).each do |n|
-    return false if number% n == 0
-  end
-  true
+  if number < 0 
+  
+  else
+    (2..(number - 1)).each do |n|
+      return false if number% n == 0
+      end
+      true
+   end
 end
